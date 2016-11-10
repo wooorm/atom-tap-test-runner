0ca70246a5e555f797dd976b7d9461a3021ccc03
Titus Wormer <tituswormer@gmail.com>
2016-11-10 09:49:31 +0100

    Increase test times

--- test/nok.js
+++ test/nok.js
@@ -8,5 +8,5 @@ test('testing atom', function (t) {
 
   t.ok('loadTime' in atom, '`atom` should have a `loadTime` prop');
   t.equal(typeof atom.loadTime, 'number', '`loadTime` should be a number');
-  t.ok(atom.loadTime > 5000, '`loadTime` should be bigger than five seconds');
+  t.ok(atom.loadTime > 10000, '`loadTime` should be bigger than five seconds');
 });
