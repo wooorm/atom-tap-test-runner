a3a6a12b5e372ce90563ff2e03ffa46bd5f7ac7b
Titus Wormer <tituswormer@gmail.com>
2019-05-05 14:56:53 +0200

    Fix tests

--- test/index.js
+++ test/index.js
@@ -5,65 +5,72 @@ var test = require('tape')
 var execa = require('execa')
 
 test('success', function(t) {
-  t.plan(2)
+  Promise.resolve()
+    .then(function() {
+      return execa('atom', ['--test', relative('ok.js')])
+    })
+    .then(function(res) {
+      t.equal(
+        res.stdout,
+        [
+          'TAP version 13',
+          '# testing atom',
+          'ok 1 `atom` should have a `loadTime` prop',
+          'ok 2 `loadTime` should be a number',
+          'ok 3 `loadTime` should be less than five seconds',
+          '',
+          '1..3',
+          '# tests 3',
+          '# pass  3',
+          '',
+          '# ok',
+          ''
+        ].join('\n'),
+        'should report successes'
+      )
+    })
+    .then(function() {
+      return execa('atom', ['--test', relative('nok.js')])
+    })
+    .catch(function(error) {
+      var res = error.stdout
+        .split('\n')
+        .filter(filterStack)
+        .join('\n')
 
-  execa('atom', ['--test', relative('ok.js')]).then(function(res) {
-    t.equal(
-      res.stdout,
-      [
-        'TAP version 13',
-        '# testing atom',
-        'ok 1 `atom` should have a `loadTime` prop',
-        'ok 2 `loadTime` should be a number',
-        'ok 3 `loadTime` should be less than five seconds',
-        '',
-        '1..3',
-        '# tests 3',
-        '# pass  3',
-        '',
-        '# ok',
-        ''
-      ].join('\n'),
-      'should report successes'
-    )
-  })
+      t.equal(
+        res,
+        [
+          'TAP version 13',
+          '# testing atom',
+          'ok 1 `atom` should have a `loadTime` prop',
+          'ok 2 `loadTime` should be a number',
+          'not ok 3 `loadTime` should be bigger than five seconds',
+          '  ---',
+          '    operator: ok',
+          '    expected: true',
+          '    actual:   false',
+          '    stack: |-',
+          '      Error: `loadTime` should be bigger than five seconds',
+          '  ...',
+          '',
+          '1..3',
+          '# tests 3',
+          '# pass  2',
+          '# fail  1',
+          '',
+          ''
+        ].join('\n'),
+        'should report failures'
+      )
 
-  execa('atom', ['--test', relative('nok.js')]).catch(function(error) {
-    var res = error.stdout
-      .split('\n')
-      .filter(filterStack)
-      .join('\n')
-
-    t.equal(
-      res,
-      [
-        'TAP version 13',
-        '# testing atom',
-        'ok 1 `atom` should have a `loadTime` prop',
-        'ok 2 `loadTime` should be a number',
-        'not ok 3 `loadTime` should be bigger than five seconds',
-        '  ---',
-        '    operator: ok',
-        '    expected: true',
-        '    actual:   false',
-        '    stack: |-',
-        '      Error: `loadTime` should be bigger than five seconds',
-        '  ...',
-        '',
-        '1..3',
-        '# tests 3',
-        '# pass  2',
-        '# fail  1',
-        '',
-        ''
-      ].join('\n'),
-      'should report failures'
-    )
-
-    function filterStack(line) {
-      return !/^\s+at/.test(line)
-    }
-  })
+      function filterStack(line) {
+        return !/^\s+at/.test(line)
+      }
+    })
+    .then(function() {
+      t.end()
+    })
 })
 
 function relative(filename) {
